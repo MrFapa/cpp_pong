0287643dc0061c101ba02da639ff9ec790417cdd
MrFapa <Mrfapadevelopement@gmail.com>
2023-07-13 12:10:16 +0200

    Added glm (MathLib)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+#include "glm/gtc/matrix_transform.hpp"
 #include "Paddle.h"
 
 int main(void)
@@ -19,36 +20,30 @@ int main(void)
         return -1;
     }
 
-    /*Shader shader("base.shader");
-    shader.Bind();*/
-
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glewInit();
 
+
+    
+
     Paddle* paddle = new Paddle(GLFW_KEY_W, GLFW_KEY_S);
 
-    unsigned int m_VB;
-    std::vector<float> m_VertexPositions = {
-        0, 0, 0, 0,
-        0, 0, 0, 0
-    };
+    glm::mat4 proj = glm::ortho(0.0f, 100.0f, 0.0f, 800.0f, -1.0f, 1.0f);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
-
-
-        //shader.Bind();
-
+        
         int keyWState = glfwGetKey(window, GLFW_KEY_W);
         int keySState = glfwGetKey(window, GLFW_KEY_S);
         paddle->ReceiveInput(keyWState);
         paddle->ReceiveInput(keySState * -1);
 
         paddle->OnUpdate(1.0f);
-        paddle->OnDraw();
+        
+    	paddle->OnDraw();
         
 
         /* Swap front and back buffers */
